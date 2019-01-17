335bcbf37ee5fa0d19555024a04ee8ea3b2c8b5c
Alexander Krupnov <dev@uniqway.ru>
2019-01-17 23:25:36 +0300

    Create WebAppInterface.java

---
+++ android/src/main/java/com/flutter_webview_plugin/WebAppInterface.java
@@ -0,0 +1,17 @@
+package com.flutter_webview_plugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WebAppInterface {
+
+    //@JavascriptInterface
+    public void exec(String command, String param) {
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("command", command);
+        data.put("param", param);
+
+        FlutterWebviewPlugin.channel.invokeMethod("exec", data);
+    }
+}
